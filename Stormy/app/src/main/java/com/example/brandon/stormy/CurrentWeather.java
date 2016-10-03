94dbada2571f62565db6d591801c03378f1caba5
Brandon Owens <brandono@vitamins2you.com>
2016-10-03 15:21:53 -0400

    Stormy

---
+++ Stormy/app/src/main/java/com/example/brandon/stormy/CurrentWeather.java
@@ -0,0 +1,61 @@
+package com.example.brandon.stormy;
+
+/**
+ * Created by Brandon on 10/3/2016.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTIme;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTIme() {
+        return mTIme;
+    }
+
+    public void setTIme(long TIme) {
+        mTIme = TIme;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
--- Stormy/app/src/main/java/com/example/brandon/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/example/brandon/stormy/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.brandon.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -26,22 +31,58 @@ public class MainActivity extends AppCompatActivity {
         String forecastUrl = "https://api.darksky.net/forecast/"+apiKey+
                 "/"+latitude+","+longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
 
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.v(TAG, response.body().string());
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, response.body().string());
+                        } else {
+                            alertUserAboutError();
+                        }
+
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "Network Unavailable!", Toast.LENGTH_LONG);
         }
 
+        Log.d(TAG, "Main UI code is running!");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
